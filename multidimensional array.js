212749ce07483c1fbeb8ea2a188dc8db37ccc348
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-03-20 21:00:41 +0530

    Add files via upload

---
+++ multidimensional array.js
@@ -0,0 +1,12 @@
+var arr=[[5,6,4],[99,87,10],[23,98,67]];
+console.log(arr[1][2]);//10
+console.log(arr[2][0]);//23
+
+var arr1=["cat","dog","noise"];
+arr1.push(["happy","join"]);
+console.log(arr1);
+
+var myArr=[["happy",1],["sad",2]];
+console.log(myArr);
+myArr.pop();
+console.log(myArr);
